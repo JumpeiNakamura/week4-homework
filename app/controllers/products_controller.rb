5f9f375eea3dea1204537ba9df2b5e52db3e79df
JumpeiNakamura <jun.n1004@gmail.com>
2014-02-01 23:13:25 -0600

    Week 4 homework
    website for hotdeals.com

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+
+  def index
+  end
+
+
+    # @product_list = ["iPhone 3", "iPhone 4", "iPhone 4s", "iPhone 5", "iPhone 5c", "iPhone 5s"]
+
+  def show
+  end
+
+  def deal1
+  end
+
+  def deal2
+  end
+
+  def deal3
+  end
+
+    # @product = "iPhone"
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Week4hw::Application.routes.draw do
 
   get "/" => "pages#home"
+  get "/about" => "pages#about"
+  get "/contactus" => "pages#contactus"
+
+  get "/weather/search" => "weather#search"
+  get "/weather/conditions" => "weather#conditions"
+
+  get "/index" => "products#index"
+  get "/show" => "products#show"
+  get "/products/deal1" => "products#deal1"
 
   # HINT: Add more routes here...
 
